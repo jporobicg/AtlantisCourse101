7726d9a7c09885139126cae418311338e05efb86
Porobic Garate, Javier Porobic (O&A, Hobart) <Javier.Porobicgarate@csiro.au>
2022-05-09 19:32:27 +1000

    update code naming

--- ReactiveAtlantis.R
+++ ReactiveAtlantis.R
@@ -4,20 +4,20 @@ library(ReactiveAtlantis)
 ## ~~~~~~~~~~~~~~~~~~~~~~ ##
 main_directory   <- '~/trunk/example/'
 output_directory <- paste0(main_directory, 'outputFolder/output')
-initial_cond.nc  <-  paste0(main_directory, '/init_NorthSea.nc')
-groups.csv       <- '/functionalGroups.csv'
-bgm.file         <- '/NorthSea.bgm'
-prm.file         <- '/01NorthSea_biol_fishing.prm'
+initial_cond.nc  <-  paste0(main_directory, '/INIT_VMPA_Jan2015.nc')
+groups.csv       <- '/SETasGroupsDem.csv'
+bgm.file         <- '/VMPA_setas.bgm'
+prm.file         <- '/VMPA_setas_biol_fishing_Trunk.prm'
 fsh.csv          <- 'your_fisheries_definition_file.csv'
-cum.depths       <- c(0, 10, 20, 50, 92, 192, 500)
+cum.depths       <- c(0, 20, 50, 100, 250, 700, 1200)
 
 ## ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ##
 ## ~          Compare previous run                ~ ##
 ## ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ##
 simulation01   <- '01'
 simulation02   <- '00'
-nc.out.current <- paste0(output_directory, simulation01, '/outputNorthSea.nc')
-nc.out.old     <- paste0(output_directory, simulation02, '/outputNorthSea.nc')
+nc.out.current <- paste0(output_directory, simulation01, '/outputSETAS.nc')
+nc.out.old     <- paste0(output_directory, simulation02, '/outputSETAS.nc')
 ## alone
 compare(nc.out.current, nc.out.old=NULL, grp.csv = groups.csv,bgm.file=bgm.file, cum.depths=cum.depths)
 ## compare with other output
@@ -26,30 +26,27 @@ compare(nc.out.current, nc.out.old, grp.csv = groups.csv,bgm.file=bgm.file, cum.
 ## ~~~~~~~~~~~~~~~~~ ##
 ## ~  Food weeb    ~ ##
 ## ~~~~~~~~~~~~~~~~~ ##
-diet.file <- paste0(output_directory, simulation01, '/outputNorthSeaDietCheck.txt'
+diet.file <- paste0(output_directory, simulation01, '/outputSETASDietCheck.txt'
 food.web(diet.file, groups.csv)
 
 ## ~~~~~~~~~~~~~~~~~~~~~~ ##
 ## ~      Recruitment   ~ ##
 ## ~~~~~~~~~~~~~~~~~~~~~~ ##
-yoy.file       <- paste0(output_directory, simulation01, '/outputNorthSeaYOY.txt')
-nc.out.current <- paste0(output_directory, simulation01, '/outputNorthSea.nc')
+yoy.file       <- paste0(output_directory, simulation01, '/outputSETASYOY.txt')
+nc.out.current <- paste0(output_directory, simulation01, '/outputSETAS.nc')
 recruitment.cal(ini.nc.file, out.nc.file, yoy.file, groups.csv, prm.file)
 
 ## ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ##
 ## ~             Growth Primary producers         ~ ##
 ## ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ##
-prm.file      <- '/home/por07g/Documents/2019/North-Sea/JPG_North_Sea_Atlantis/NS_model/00NorthSea_biol_fishing.prm'
-out.nc.file   <- paste0('/home/por07g/Documents/2019/North-Sea/JPG_North_Sea_Atlantis/NS_model/NorthSea_Output_Folder/output', sim, '/outputNorthSea.nc')
-#debug(growth.pp)
 growth.pp(ini.nc.file, groups.csv, prm.file, out.nc.file)
 
 ## ~~~~~~~~~~~~~~~~~~ ##
 ## ~     Predation  ~ ##
 ## ~~~~~~~~~~~~~~~~~~ ##
-biomass   <- paste0(output_directory, simulation01, '/outputNorthSeaBiomIndx.txt')
-diet.file <- paste0(output_directory, simulation01, '/outputNorthSeaDietCheck.txt')
-bio.age   <- paste0(output_directory, simulation01, '/outputNorthSeaAgeBiomIndx.txt')
+biomass   <- paste0(output_directory, simulation01, '/outputSETASBiomIndx.txt')
+diet.file <- paste0(output_directory, simulation01, '/outputSETASDietCheck.txt')
+bio.age   <- paste0(output_directory, simulation01, '/outputSETASAgeBiomIndx.txt')
 
 predation(biom,grp.csv, diet.file, age.biomass=bio.age)
 
